0c8e894303029bb1437a77aa6773e362beef1b99
PriyankDewashish <162489178+PriyankDewashish@users.noreply.github.com>
2024-03-06 23:41:23 +0530

    Add files via upload

---
+++ quiz app/questions.js
@@ -0,0 +1,112 @@
+let questions = [
+    {
+        numb: 1,
+        question: "Who programmed the first computer game 'Spacewar!' in 1962?",
+        answer: "A. Steave Russell",
+        options: [
+            "A. Steave Russell",
+            "B. Konard Zuse",
+            "C. Alan Emtage",
+            "D. Tim Berners-Lee"
+        ]
+    },
+    {
+        numb: 2,
+        question: "Who is known as the father of supercomputing?",
+        answer: "D. Seymour Cray",
+        options: [
+            "A. David J. Brown",
+            "B. Gene Amdahl",
+            "C. Adam Dunkels",
+            "D. Seymour Cray"
+        ]
+    },
+    {
+        numb: 3,
+        question: "Who created the C programming Language?",
+        answer: "B. Dennis Ritchie",
+        options: [
+            "A. Ken Thompson",
+            "B. Dennis Ritchie",
+            "C. Robin Milner",
+            "D. Frieder Nake"
+        ]
+    },
+    {
+        numb: 4,
+        question: "Who is known as the father of internet?",
+        answer: "C. Vint Cerf",
+        options: [
+            "A. Alan Perlis",
+            "B. Jean E. Sammet",
+            "C. Vint Cerf",
+            "D. Steve Lawrence"
+        ]
+    },
+    {
+        numb: 5,
+        question: "Which one is the first high level programming language?",
+        answer: "C. FORTRAN",
+        options: [
+            "A. C",
+            "B. COBOL",
+            "C. FORTRAN",
+            "D. C++"
+        ]
+    },
+    {
+        numb: 6,
+        question: "India's first Super Computer PARAM 8000 was installed in",
+        answer: "B. 1991",
+        options: [
+            "A. 1990",
+            "B. 1991",
+            "C. 1992",
+            "D. 1993"
+        ]
+    },
+    {
+        numb: 7,
+        question: "Who developed Java Programming Language?",
+        answer: "A. James A. Gosling",
+        options: [
+            "A. James A. Gosling",
+            "B. Brendan Eich",
+            "C. Bjarne Stroustrup",
+            "D. Guido van Rossum"
+        ]
+    },
+    {
+        numb: 8,
+        question: "Which one is volatile memory in a computer system?",
+        answer: "B. RAM",
+        options: [
+            "A. ROM",
+            "B. RAM",
+            "C. Hard Disk",
+            "D. Floppy Disk"
+        ]
+    },
+    {
+        numb: 9,
+        question: "One Terabyte(1 TB) is equal to?",
+        answer: "D. 1024 GB",
+        options: [
+            "A. 1000 GB",
+            "B. 512 GB",
+            "C. 1024 MB",
+            "D. 1024 GB"
+        ]
+    },
+    {
+        numb: 10,
+        question: "Who first developed QWERTY keyboard used in computers and phones?",
+        answer: "C. Christopher Latham Sholes",
+        options: [
+            "A. James A. Gosling",
+            "B. Win Ebbinkhuijsen",
+            "C. Christopher Latham Sholes",
+            "D. Shafi Goldwasser"
+        ]
+    }
+]
